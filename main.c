d8e0ec4319b60a34a844751cb833018477dc13ec
kavi742 <kavimehta18@protonmail.com>
2023-10-30 15:49:44 -0400

    Created 2 implmentations of read_line function

--- main.c
+++ main.c
@@ -3,8 +3,77 @@
 @date 2023-10-09
 @file main.c
 *******************/
+
+//Read line buffer size
+#define KSH_RL_BUFSIZE 1024
+
 /**
-Loop that takes input, reads, parses, and executes the input
+ * Readline function implemented with getline()
+ * Input :
+ * Output : char line
+ */
+char *ksh_read_line(void){
+  char *line = NULL;
+  //getline allocates buffer
+  ssize_t bufsize = 0;
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    // Got to EOF
+    if (feof(stdin)){
+      exit(EXIT_SUCCESS);
+    } else {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+}
+
+/**
+ * For learning purposes don't use getline()
+ * Read line function implemented without getline()
+ * Input : 
+ * Output : char buffer 
+*/
+char *ksh_read_line_without_get_line(void){
+  int bufsize = KSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char)*bufsize);
+  // use int here because EOF is an int not char so comparison only works if c is int
+  int c;
+
+  if(!buffer){
+    fprintf(stderr,"ksh: allocation error\n")
+  }
+
+  while(1){
+    // Get character from user input
+    c = getchar();
+
+    // If EOF append null character and return 
+    if (c == EOF || c == '\n'){
+      buffer[position] = '\0';
+      return buffer
+    }
+    // else append char c to buffer and loop
+    else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // if buffer is exceeded then reallocate
+    if (position >= buffer){
+      bufsize = KSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer){
+        fprintf(stderr, "ksh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+/**
+ * Loop that takes input, reads, parses, and executes the input
  */
 void ksh_loop(void){
   char *line;
